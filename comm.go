73b338aa6dbca0dfab73fe9d185774707b2e06e9
vyzo <vyzo@hackzen.org>
2018-08-28 21:17:00 +0300

    fix nil pointer dereference in copyRPC

--- comm.go
+++ comm.go
@@ -124,17 +124,20 @@ func rpcWithControl(msgs []*pb.Message,
 }
 
 func copyRPC(rpc *RPC) *RPC {
-	return &RPC{
+	res := &RPC{
 		RPC: pb.RPC{
 			Subscriptions: rpc.Subscriptions,
 			Publish:       rpc.Publish,
-			Control: &pb.ControlMessage{
-				Ihave: rpc.Control.Ihave,
-				Iwant: rpc.Control.Iwant,
-				Graft: rpc.Control.Graft,
-				Prune: rpc.Control.Prune,
-			},
 		},
 		from: rpc.from,
 	}
+	if rpc.Control != nil {
+		res.Control = &pb.ControlMessage{
+			Ihave: rpc.Control.Ihave,
+			Iwant: rpc.Control.Iwant,
+			Graft: rpc.Control.Graft,
+			Prune: rpc.Control.Prune,
+		}
+	}
+	return res
 }
