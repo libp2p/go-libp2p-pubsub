f6ac3aab4faeaec37731c88b7520a9de15c8af20
vyzo <vyzo@hackzen.org>
2019-01-17 14:20:34 +0200

    lru blackclist

--- blacklist.go
+++ blacklist.go
@@ -1,6 +1,7 @@
 package pubsub
 
 import (
+	lru "github.com/hashicorp/golang-lru"
 	peer "github.com/libp2p/go-libp2p-peer"
 )
 
@@ -25,4 +26,28 @@ func (b MapBlacklist) Add(p peer.ID) {
 func (b MapBlacklist) Contains(p peer.ID) bool {
 	_, ok := b[p]
 	return ok
+}
+
+// LRUBlacklist is a blacklist implementation using an LRU cache
+type LRUBlacklist struct {
+	lru *lru.Cache
+}
+
+// NewLRUBlacklist creates a new LRUBlacklist with capacity cap
+func NewLRUBlacklist(cap int) (Blacklist, error) {
+	c, err := lru.New(cap)
+	if err != nil {
+		return nil, err
+	}
+
+	b := &LRUBlacklist{lru: c}
+	return b, nil
+}
+
+func (b LRUBlacklist) Add(p peer.ID) {
+	b.lru.Add(p, nil)
+}
+
+func (b LRUBlacklist) Contains(p peer.ID) bool {
+	return b.lru.Contains(p)
 }
