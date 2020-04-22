92867faf00eb47d4433a96b6591b2899f4d97037
Yusef Napora <yusef@protocol.ai>
2020-04-22 11:59:43 -0400

    add TestScoreFirstMessageDeliveriesDecay

--- score_test.go
+++ score_test.go
@@ -163,6 +163,58 @@ func TestScoreFirstMessageDeliveriesCap(t *testing.T) {
 	}
 }
 
+func TestScoreFirstMessageDeliveriesDecay(t *testing.T) {
+	// Create parameters with reasonable default values
+	mytopic := "mytopic"
+	params := &PeerScoreParams{
+		AppSpecificScore: func(peer.ID) float64 { return 0 },
+		Topics:           make(map[string]*TopicScoreParams),
+	}
+	topicScoreParams := &TopicScoreParams{
+		TopicWeight:                  1,
+		FirstMessageDeliveriesWeight: 1,
+		FirstMessageDeliveriesDecay:  0.9, // decay 10% per decay interval
+		FirstMessageDeliveriesCap:    2000,
+		TimeInMeshQuantum:            time.Second, // bug? not setting this causes a div by zero
+	}
+
+	params.Topics[mytopic] = topicScoreParams
+	peerA := peer.ID("A")
+
+	ps := newPeerScore(params)
+	ps.AddPeer(peerA, "myproto")
+	ps.Graft(peerA, mytopic)
+
+	// deliver a bunch of messages from peer A
+	nMessages := 100
+	for i := 0; i < nMessages; i++ {
+		pbMsg := makeTestMessage(i)
+		pbMsg.TopicIDs = []string{mytopic}
+		msg := Message{ReceivedFrom: peerA, Message: pbMsg}
+		ps.ValidateMessage(&msg)
+		ps.DeliverMessage(&msg)
+	}
+
+	ps.refreshScores()
+	aScore := ps.Score(peerA)
+	expected := topicScoreParams.TopicWeight * topicScoreParams.FirstMessageDeliveriesWeight * topicScoreParams.FirstMessageDeliveriesDecay * float64(nMessages)
+	variance := 0.1
+	if !withinVariance(aScore, expected, variance) {
+		t.Fatalf("Score: %f. Expected %f ± %f", aScore, expected, variance*expected)
+	}
+
+	// refreshing the scores applies the decay param, so applying twice should
+	decayIntervals := 10
+	for i := 0; i < decayIntervals; i++ {
+		ps.refreshScores()
+		expected *= topicScoreParams.FirstMessageDeliveriesDecay
+	}
+	aScore = ps.Score(peerA)
+	if !withinVariance(aScore, expected, variance) {
+		t.Fatalf("Score: %f. Expected %f ± %f", aScore, expected, variance*expected)
+	}
+}
+
 func withinVariance(score float64, expected float64, variance float64) bool {
 	return score > expected*(1-variance) && score < expected*(1+variance)
 }
