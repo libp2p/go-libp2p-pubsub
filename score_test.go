d10066977a2436851ffe5ca4ffbb15e7f915fe5c
Yusef Napora <yusef@protocol.ai>
2020-04-22 11:40:06 -0400

    add TestScoreFirstMessageDeliveries

--- score_test.go
+++ score_test.go
@@ -81,6 +81,54 @@ func TestScoreTimeInMeshCap(t *testing.T) {
 	}
 }
 
+func TestScoreFirstMessageDeliveries(t *testing.T) {
+	// Create parameters with reasonable default values
+	mytopic := "mytopic"
+	params := &PeerScoreParams{
+		AppSpecificScore: func(peer.ID) float64 { return 0 },
+		Topics:           make(map[string]*TopicScoreParams),
+	}
+	topicScoreParams := &TopicScoreParams{
+		TopicWeight:       1,
+		FirstMessageDeliveriesWeight:  1,
+		FirstMessageDeliveriesDecay: 1.0, // test without decay for now
+		FirstMessageDeliveriesCap: 2000,
+		TimeInMeshQuantum: time.Second, // bug? not setting this causes a div by zero
+
+		// set all other weights to zero
+		MeshMessageDeliveriesWeight: 0,
+		TimeInMeshWeight: 0,
+		MeshFailurePenaltyWeight: 0,
+		InvalidMessageDeliveriesWeight: 0,
+	}
+
+	params.Topics[mytopic] = topicScoreParams
+	peerA := peer.ID("A")
+
+	ps := newPeerScore(params)
+	ps.AddPeer(peerA, "myproto")
+	ps.Graft(peerA, mytopic)
+
+
+	// deliver a bunch of messages from peer A
+	nMessages := 100
+	for i := 0; i < nMessages; i++ {
+		pbMsg := makeTestMessage(i)
+		pbMsg.TopicIDs = []string{mytopic}
+		msg := Message{ReceivedFrom: peerA, Message: pbMsg}
+		ps.ValidateMessage(&msg)
+		ps.DeliverMessage(&msg)
+	}
+
+	ps.refreshScores()
+	aScore := ps.Score(peerA)
+	expected := topicScoreParams.TopicWeight * topicScoreParams.FirstMessageDeliveriesWeight * float64(nMessages)
+	variance := 0.5
+	if !withinVariance(aScore, expected, variance) {
+		t.Fatalf("Score: %f. Expected %f ± %f", aScore, expected, variance*expected)
+	}
+}
+
 func withinVariance(score float64, expected float64, variance float64) bool {
 	return score > expected*(1-variance) && score < expected*(1+variance)
 }
