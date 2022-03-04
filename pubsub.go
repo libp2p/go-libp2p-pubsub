94b386960ce69587b403c1668609a585d6a090c5
Iulian Pascalau <iulian.pascalau@gmail.com>
2022-03-04 19:56:38 +0200

    larger size to 2MB

--- pubsub.go
+++ pubsub.go
@@ -23,8 +23,8 @@ import (
 	timecache "github.com/whyrusleeping/timecache"
 )
 
-// DefaultMaximumMessageSize is 1mb.
-const DefaultMaxMessageSize = 1 << 20
+// DefaultMaxMessageSize is 2MB
+const DefaultMaxMessageSize = 1 << 21
 
 var (
 	TimeCacheDuration = 120 * time.Second
