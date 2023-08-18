7da7423be7caaaaf434ff58b7db14de5b5ab6d03
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-08-18 17:23:45 +0530

    fix: topicscore params can't be set for a topic subscribed after gossipsub is initialized

--- score.go
+++ score.go
@@ -202,12 +202,8 @@ func (ps *peerScore) SetTopicScoreParams(topic string, p *TopicScoreParams) erro
 	ps.Lock()
 	defer ps.Unlock()
 
-	old, exist := ps.params.Topics[topic]
+	old, _ := ps.params.Topics[topic]
 	ps.params.Topics[topic] = p
-
-	if !exist {
-		return nil
-	}
 
 	// check to see if the counter Caps are being lowered; if that's the case we need to recap them
 	recap := false
