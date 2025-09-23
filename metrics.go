cdf3c1e679856f3aae8077d18d2b1b002b8928d2
Bharath Vedartham <vedabharath12345@gmail.com>
2025-09-23 13:40:20 +0530

    prefix metrics

--- metrics.go
+++ metrics.go
@@ -8,6 +8,8 @@ import (
 	"go.opentelemetry.io/otel/metric"
 )
 
+const metricPrefix = "libp2p_gossipsub_"
+
 type metrics struct {
 	metric.MeterProvider
 	// TODO
@@ -93,7 +95,7 @@ func InitMetrics(ps *PubSub) error {
 
 	var err error
 	ps.metrics.messageProcessingTime, err = meter.Int64Histogram(
-		"msg_processing_time.duration",
+		metricPrefix+"msg_processing_time.duration",
 		metric.WithDescription("The duration of message processing not including validation"),
 		metric.WithUnit("us"),
 		metric.WithExplicitBucketBoundaries(100, 500, 1_000, 5_000, 10_000, 50_000, 100_000, 250_000, 500_000, 1_000_000, 5_000_000, 10_000_000),
@@ -103,7 +105,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.messageParsingTime, err = meter.Int64Histogram(
-		"message_parsing_time.duration",
+		metricPrefix+"message_parsing_time.duration",
 		metric.WithDescription("The duration of parsing a message received from the network stream to an RPC"),
 		metric.WithUnit("us"),
 		metric.WithExplicitBucketBoundaries(100, 500, 1_000, 5_000, 10_000, 50_000, 100_000, 250_000, 500_000, 1_000_000, 5_000_000, 10_000_000),
@@ -113,7 +115,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.heartbeatTime, err = meter.Int64Histogram(
-		"heartbeat_time.duration",
+		metricPrefix+"heartbeat_time.duration",
 		metric.WithDescription("The duration of the heartbeat"),
 		metric.WithUnit("us"),
 		metric.WithExplicitBucketBoundaries(100, 500, 1_000, 5_000, 10_000, 50_000, 100_000, 250_000, 500_000, 1_000_000, 5_000_000, 10_000_000),
@@ -123,7 +125,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.totalPeerCount, err = meter.Int64Gauge(
-		"total_peer_count",
+		metricPrefix+"total_peer_count",
 		metric.WithDescription("The total number of peers that are connected to this pubsub router"),
 	)
 	if err != nil {
@@ -131,7 +133,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.totalTopicCount, err = meter.Int64Gauge(
-		"total_topic_count",
+		metricPrefix+"total_topic_count",
 		metric.WithDescription("The total number of topics that are subscribed to this pubsub router"),
 	)
 	if err != nil {
@@ -139,7 +141,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.totalSubscriptionCount, err = meter.Int64Gauge(
-		"total_subscription_count",
+		metricPrefix+"total_subscription_count",
 		metric.WithDescription("The total number of subscriptions that are active in this pubsub router"),
 	)
 	if err != nil {
@@ -147,7 +149,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.incomingQueueDepth, err = meter.Int64Gauge(
-		"incoming_queue_depth",
+		metricPrefix+"incoming_queue_depth",
 		metric.WithDescription("The depth of the incoming queue in the event loop"),
 	)
 	if err != nil {
@@ -155,7 +157,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.sendMsgQueueDepth, err = meter.Int64Gauge(
-		"sendmsg_queue_depth",
+		metricPrefix+"sendmsg_queue_depth",
 		metric.WithDescription("The depth of the send message queue in the event loop"),
 	)
 	if err != nil {
@@ -163,7 +165,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.messageSize, err = meter.Int64Histogram(
-		"message_size",
+		metricPrefix+"message_size",
 		metric.WithDescription("The size of the messages received by the peer in bytes"),
 		metric.WithUnit("bytes"),
 	)
@@ -172,7 +174,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.meshMemberCount, err = meter.Int64Gauge(
-		"mesh_member_count",
+		metricPrefix+"mesh_member_count",
 		metric.WithDescription("The number of mesh members per topic"),
 	)
 	if err != nil {
@@ -180,7 +182,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.fanoutMemberCount, err = meter.Int64Gauge(
-		"fanout_member_count",
+		metricPrefix+"fanout_member_count",
 		metric.WithDescription("The number of fanout members per topic"),
 	)
 	if err != nil {
@@ -188,7 +190,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.eventLoopWaitTime, err = meter.Int64Histogram(
-		"event_loop_wait_time",
+		metricPrefix+"event_loop_wait_time",
 		metric.WithDescription("The time spent by different events in the event loop before being picked up for processing"),
 		metric.WithUnit("us"),
 	)
@@ -197,7 +199,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.eventProcessingTime, err = meter.Int64Histogram(
-		"event_processing_time",
+		metricPrefix+"event_processing_time",
 		metric.WithDescription("The time spent processing different events in the event loop"),
 		metric.WithUnit("us"),
 	)
@@ -206,7 +208,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.iDontWantMsgRecvd, err = meter.Int64Counter(
-		"idont_want_msg_recvd",
+		metricPrefix+"idont_want_msg_recvd",
 		metric.WithDescription("The number of times we have received an IDONTWANT message"),
 	)
 	if err != nil {
@@ -214,7 +216,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.iDontWantMsgSent, err = meter.Int64Counter(
-		"idont_want_msg_sent",
+		metricPrefix+"idont_want_msg_sent",
 		metric.WithDescription("The number of times we have sent an IDONTWANT message"),
 	)
 	if err != nil {
@@ -222,7 +224,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.iWantMsgSent, err = meter.Int64Counter(
-		"iwant_msg_sent",
+		metricPrefix+"iwant_msg_sent",
 		metric.WithDescription("The number of IWANT msgs sent"),
 	)
 	if err != nil {
@@ -230,7 +232,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.iWantMsgRecvd, err = meter.Int64Counter(
-		"iwant_msg_recvd",
+		metricPrefix+"iwant_msg_recvd",
 		metric.WithDescription("The number of IWANT msgs received"),
 	)
 	if err != nil {
@@ -238,7 +240,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.pruneMsgRecvdPerTopic, err = meter.Int64Counter(
-		"prune_msg_recvd_per_topic",
+		metricPrefix+"prune_msg_recvd_per_topic",
 		metric.WithDescription("The number of prune msgs received per topic"),
 	)
 	if err != nil {
@@ -246,7 +248,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.graftMsgRecvdPerTopic, err = meter.Int64Counter(
-		"graft_msg_recvd_per_topic",
+		metricPrefix+"graft_msg_recvd_per_topic",
 		metric.WithDescription("The number of graft msgs received per topic"),
 	)
 	if err != nil {
@@ -254,7 +256,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.pruneMsgSentPerTopic, err = meter.Int64Counter(
-		"prune_msg_sent_per_topic",
+		metricPrefix+"prune_msg_sent_per_topic",
 		metric.WithDescription("The number of prune msgs sent per topic"),
 	)
 	if err != nil {
@@ -262,7 +264,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.graftMsgSentPerTopic, err = meter.Int64Counter(
-		"graft_msg_sent_per_topic",
+		metricPrefix+"graft_msg_sent_per_topic",
 		metric.WithDescription("The number of graft msgs sent per topic"),
 	)
 	if err != nil {
@@ -270,7 +272,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicMsgSent, err = meter.Int64Counter(
-		"topic_msg_sent",
+		metricPrefix+"topic_msg_sent",
 		metric.WithDescription("The number of messages sent per topic"),
 	)
 	if err != nil {
@@ -278,7 +280,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicBytesSent, err = meter.Int64Counter(
-		"topic_bytes_sent",
+		metricPrefix+"topic_bytes_sent",
 		metric.WithDescription("The bytes sent per topic"),
 	)
 	if err != nil {
@@ -286,7 +288,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicMsgPublished, err = meter.Int64Counter(
-		"topic_msg_published",
+		metricPrefix+"topic_msg_published",
 		metric.WithDescription("The number of messages published per topic"),
 	)
 	if err != nil {
@@ -294,7 +296,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicMsgRecvd, err = meter.Int64Counter(
-		"topic_msg_recvd",
+		metricPrefix+"topic_msg_recvd",
 		metric.WithDescription("The number of messages received per topic"),
 	)
 	if err != nil {
@@ -302,7 +304,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicBytesRecvd, err = meter.Int64Counter(
-		"topic_bytes_recvd",
+		metricPrefix+"topic_bytes_recvd",
 		metric.WithDescription("The bytes received per topic"),
 	)
 	if err != nil {
@@ -310,7 +312,7 @@ func InitMetrics(ps *PubSub) error {
 	}
 
 	ps.metrics.topicMsgRecvdUnfiltered, err = meter.Int64Counter(
-		"topic_msg_recvd_unfiltered",
+		metricPrefix+"topic_msg_recvd_unfiltered",
 		metric.WithDescription("The number of messages received per topic unfiltered(with duplicates)"),
 	)
 	if err != nil {
