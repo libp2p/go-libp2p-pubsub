788baeee7389812c2e332b0c9956034efef00c25
Marco Munizaga <git@marcopolo.io>
2026-05-29 15:20:23 -0700

    Count message publish overhead as control bytes
    Bytes that aren't part of the message data now contribute to the control
    byte limit.

--- pb/validator.go
+++ pb/validator.go
@@ -29,15 +29,53 @@ func ValidateRawRPCControlMessageSize(msg []byte, maxControlMessageSize int) err
 		}
 
 		switch field {
-		case rpcPublishFieldNumber, rpcPartialFieldNumber:
+		case rpcPartialFieldNumber:
+			// There can only be a single partial message per rpc. So the
+			// overhead size is small per RPC.
+		case rpcPublishFieldNumber:
+			payloadLen, err := dataFieldLength(fieldValue)
+			if err != nil {
+				return err
+			}
+			// Add the message overhead as part of the control size
+			controlSize += tagLen + fieldLen - payloadLen
+
 		default:
 			controlSize += tagLen + fieldLen
-			if controlSize > maxControlMessageSize {
-				return fmt.Errorf("rpc control size exceeds max control message size: %d > %d", controlSize, maxControlMessageSize)
-			}
 		}
-
 		msg = fieldValue[fieldLen:]
+
+		if controlSize > maxControlMessageSize {
+			return fmt.Errorf("rpc control size exceeds max control message size: %d > %d", controlSize, maxControlMessageSize)
+		}
 	}
 	return nil
+}
+
+func dataFieldLength(msg []byte) (int, error) {
+	const dataFieldNumber protowire.Number = 2
+	msg, n := protowire.ConsumeBytes(msg)
+	if n < 0 {
+		return 0, protowire.ParseError(n)
+	}
+
+	for len(msg) > 0 {
+		field, typ, tagLen := protowire.ConsumeTag(msg)
+		if tagLen < 0 {
+			return 0, protowire.ParseError(tagLen)
+		}
+
+		fieldValue := msg[tagLen:]
+		fieldLen := protowire.ConsumeFieldValue(field, typ, fieldValue)
+		if fieldLen < 0 {
+			return 0, protowire.ParseError(fieldLen)
+		}
+		msg = fieldValue[fieldLen:]
+
+		if field == dataFieldNumber && typ == protowire.BytesType {
+			return fieldLen, nil
+		}
+	}
+
+	return 0, nil
 }
